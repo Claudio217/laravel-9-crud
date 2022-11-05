76d1c810f4abad8fcbf2a4d618e82c4e410f366c
Claudio217 <111467423+Claudio217@users.noreply.github.com>
2022-11-05 15:01:38 -0600

    TODO BIEN HASTA EL PASO 4
    ERRORES SOLUCIONADOS AHORA SI

--- database/migrations/2022_10_29_185010_create_companies_table.php
+++ database/migrations/2022_10_29_185010_create_companies_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::create('companies', function (Blueprint $table) {
         $table->id();
-            $table->timestamps();
+        $table->timestamps();
+    });
     }
 
     /**
